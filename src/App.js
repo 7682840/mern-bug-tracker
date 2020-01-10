51fa4c1d3817e96048ab5de7f0eaa778ac13c659
7682840 <7682840@student.swin.edu.au>
2020-01-10 15:36:02 +1100

    Added bootstrap and developed initial page with navbar and logo

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './images/icons8-bug-16.png';
+import './App.css';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="https://icons8.com/icons/set/bug" target="_blank">
+          <img src="https://img.icons8.com/office/40/000000/bug.png"/>
+        </a>
+        <a class="navbar-brand" href="#"><strong>Tracker</strong></a>
+      </nav>
+      <a target="_blank" href="https://icons8.com/icons/set/bug">Bug icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+    </div>
+  );
+}
+
+export default App;
